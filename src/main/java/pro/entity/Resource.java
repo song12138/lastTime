62a8a329f9533287a5f4bac65a02a82639726d73
songxiangfu <784413317@qq.com>
2017-08-10 16:42:10 +0800

    shiro简单登录

---
+++ src/main/java/pro/entity/Resource.java
@@ -0,0 +1,216 @@
+package pro.entity;
+
+/**
+ * @Description 资源
+ * @author william [yeemin_shon@163.com]
+ * @Date 2017/6/16 0016 11:43
+ */
+public class Resource {
+    private static final long serialVersionUID = -5764552276491009585L;
+    private String name; //资源名称
+//    private ResourceType type = ResourceType.menu; //资源类型
+    private String type; //资源类型
+    private String url; //资源路径
+    private String permission; //权限字符串
+    private String parentIds; //父编号列表
+    private Boolean available = Boolean.FALSE;
+    private String sort;
+    /**
+     * treegrid相关字段
+     */
+    private Integer level;
+    private Boolean leaf;
+    private Boolean loaded = Boolean.TRUE;
+    private Boolean expanded = Boolean.TRUE;
+
+//    public static enum ResourceType {
+//        menu("菜单"), button("按钮");
+//
+//        private final String info;
+//        private ResourceType(String info) {
+//            this.info = info;
+//        }
+//
+//        public String getInfo() {
+//            return info;
+//        }
+//    }
+
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public ResourceType getType() {
+//        return type;
+//    }
+
+//    public String getTypeEn() {
+//        return type.name();
+//    }
+
+//    public void setType(ResourceType type) {
+//        this.type = type;
+//    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getParentIds() {
+        return parentIds;
+    }
+
+    public void setParentIds(String parentIds) {
+        this.parentIds = parentIds;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+
+
+
+
+    /*public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getParentIds() {
+        return parentIds;
+    }
+
+    public void setParentIds(String parentIds) {
+        this.parentIds = parentIds;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public boolean isRootNode() {
+        return parent == "0";
+    }
+
+    public String makeSelfAsParentIds() {
+        return getParentIds() + getId() + "/";
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+//        if (4 == level) {
+//            leaf = true;
+//        }
+    }
+
+    public Boolean getLeaf() {
+        return leaf;
+    }
+
+    public void setLeaf(Boolean leaf) {
+        this.leaf = leaf;
+    }
+
+    public Boolean getLoaded() {
+        return loaded;
+    }
+
+    public Boolean getExpanded() {
+        return expanded;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resource resource = (Resource) o;
+
+        if (id != null ? !id.equals(resource.id) : resource.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Resource{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                ", permission='" + permission + '\'' +
+                ", parent=" + parent +
+                ", parentIds='" + parentIds + '\'' +
+                ", available=" + available +
+                '}';
+    }*/
+}
---
+++ src/main/java/pro/entity/Role.java
@@ -0,0 +1,164 @@
+package pro.entity;
+
+import org.apache.ibatis.type.Alias;
+
+import java.util.List;
+
+/**
+ * @Description 角色
+ * @author william [yeemin_shon@163.com]
+ * @Date 2017/6/16 0016 11:43
+ */
+@Alias("Role")
+public class Role {
+
+    private static final long serialVersionUID = -4699178964423143927L;
+
+    private String role; //角色标识 程序中判断使用,如"admin"
+
+    private String ename;
+
+    private String description; //角色描述,UI界面显示使用
+
+    private List<String> resourceIds; //拥有的资源
+
+    private Boolean available = Boolean.FALSE; //是否可用,如果不可用将不会添加给用户
+
+    public Role() {
+    }
+
+    public Role(String role, String description, Boolean available) {
+        this.role = role;
+        this.description = description;
+        this.available = available;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getResourceIds() {
+        return resourceIds;
+    }
+
+    public void setResourceIds(List<String> resourceIds) {
+        this.resourceIds = resourceIds;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    /*public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getResourceIds() {
+        if (CollectionUtil.isEmpty(resourceIds)) {
+            resourceIds = new TList<String>();
+        }
+        return resourceIds.toString();
+    }
+
+    public List<String> getResourceIdList() {
+        if (CollectionUtil.isEmpty(resourceIds)) {
+            resourceIds = new TList<String>();
+        }
+        return resourceIds;
+    }
+
+    public void setResourceIds(String resourceIds) {
+        getResourceIdList();
+        String[] resourceIdArray = resourceIds.split(",");
+        for (String resourceId : resourceIdArray) {
+            if (StringUtil.isNotEmpty(resourceId)) {
+                this.resourceIds.add(resourceId);
+            }
+        }
+    }
+
+    public void setResourceIdList(List<String> resourceIds) {
+        this.resourceIds = resourceIds;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                ", description='" + description + '\'' +
+                ", resourceIds=" + resourceIds +
+                ", available=" + available +
+                '}';
+    }*/
+}
